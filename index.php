fe1d5eea245abb112afeb10385a9b7fb5a5ec169
wenxin shi <ws86@duke.edu>
2013-12-06 18:22:33 -0500

    add LoginCheck.php

--- index.php
+++ index.php
@@ -23,22 +23,9 @@
                 <h1>Animal Kindom</h1>
             </div>
         </div>
+       
 
-        <div id="Login">
-            <div class="title">
-            	<h2>User Login</h2>
-            </div>
-
-            <form class="loginForm" action="login.php" method="post">
-                <input class="fillIn" name="username" type="text" value="User name" onfocus="if(this.value=='User name') this.value=''" onBlur="if(this.value=='') this.value='User name'" >
-                <input class="fillIn" name="password" type="text" value="Password" onfocus="if(this.value=='Password') this.value=''" onBlur="if(this.value=='') this.value='Password'" >
-                <input type="submit" value="Sign In">               
-            </form>
-              
-            <div id="Register">
-                <span>Not yet a Member? <a href=SignUp.html>Register Now</a></a></span>
-            </div>
-        </div>
+        <?php include 'LoginCheck.php';?>
 
         <div id="Search">
             <form action="Search.php" method="get">
@@ -46,25 +33,6 @@
                 <input type="submit" value="Search">
             </form>
         </div>
-    </div>
-    <div id="userlogin">
-        <?php
-
-        //This will start a session
-        session_start();
-
-        $username = $_SESSION['username'];
-
-
-        //Check do we have username and password
-        if(!$username ){
-        echo "Welcome Guest! <br> <a href=login.php>Login</a> | <a href=register.php>Register</a>";}
-        else{
-        echo "Welcome ".$username." (<a href=logout.php>Logout</a>)";
-        }
-
-
-        ?> 
     </div>
 
 </body>
