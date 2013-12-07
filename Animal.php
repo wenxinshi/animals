41b26e9e18ad4178cb58d42c018fabad91c222c3
wucangji <wucangji@gmail.com>
2013-12-06 21:36:27 -0500

    animal

--- Animal.php
+++ Animal.php
@@ -1,6 +1,18 @@
 <?php include 'layer/Header.php';?>
 <?php include 'layer/Top.php';?>
 <?php include_once 'query/Animal_Sql.php';?>
+<?php 
+    
+    
+    if(isset($_SESSION['userID'])){
+    $userID=$_SESSION['userID'];
+    $username=$_SESSION['username'];
+    $speicesID=$_GET('Id');
+    $sql="INSERT INTO visited (userID,speciesID) VALUES
+    ('$userID','$speciesID')";
+    $insert=mysqli_query($con,$sql) or die (mysqli_error());};
+  
+?>   
 <?php include 'layer/MainContent.php';?>
 <?php include 'layer/Recommand.php';?>
 <?php include 'layer/Footer.php';?>
