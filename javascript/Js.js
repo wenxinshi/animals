213c55c1652dd38a07fe8e372547960b419c71f1
wenxin shi <ws86@duke.edu>
2013-12-07 13:32:44 -0500

    add tag

--- javascript/Js.js
+++ javascript/Js.js
@@ -2,3 +2,37 @@ function SignUp(){
 window.open("SignUp.html");
 }
 
+
+function TagEdit(){
+    document.getElementById("TextArea").disabled=false;
+    document.getElementById("TagEditButton").style.display='none';
+    document.getElementById("TagEditingButton").innerHTML='<button id="TagCancelButton" type="button">Cancel</button><button id="TagSaveButton" type="button">Save</button>';
+}
+
+function showHint(str)
+{
+var xmlhttp;
+if (str.length==0)
+  { 
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","gethint.asp?q="+str,true);
+xmlhttp.send();
+}
+
--- layer/Tag.php
+++ layer/Tag.php
@@ -3,7 +3,10 @@
   	return
 ?>
 
-<div id="Tag">
-	<h2>Tag</h2>
+<div>
+	<div id="Tag"><textarea id="TextArea" disabled="true"></textarea></div>
+	<button id="TagEditButton" type="button" onclick="TagEdit()" >Edit Tag</button>
+	<div id="TagEditingButton"></div>
+
 </div>
 
