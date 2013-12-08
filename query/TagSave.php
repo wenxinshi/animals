d3b6482d4167729448738d922a44089c8c861767
wenxin shi <ws86@duke.edu>
2013-12-08 13:48:40 -0500

    tag edit done

--- query/TagSave.php
+++ query/TagSave.php
@@ -8,9 +8,7 @@ $tag=$_GET['tag'];
 $sql="INSERT INTO tags (VID, tag) VALUES ((SELECT VID FROM visited where userID =".$userID." AND speciesID =".$speciesID." ORDER BY time DESC LIMIT 1), '".$tag."')";
 
 mysqli_query($con,$sql);
-echo $sql;
-// $row=mysqli_fetch_array($result);
-// $TagContent=$row['tag'];
-// echo $TagContent;
+echo $tag;
+
 
 ?> 
